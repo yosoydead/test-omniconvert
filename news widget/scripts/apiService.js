189aa3efeed8aaa19b9308df70cbe945e769073a
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2022-05-12 11:51:34 +0300

    initial setup for api client

---
+++ news widget/scripts/apiService.js
@@ -0,0 +1,49 @@
+
+export class ApiService {
+    _newsUrl;
+    _newsData;
+    _fetchDataCallback;
+
+    constructor(url, fetchDataCallback = () => {}) {
+        const urlPattern = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+        const regex = new RegExp(urlPattern);
+
+        if((!url && typeof url !== "string") || url === "") {
+            throw new Error("No url provided in the constructor.");
+        }
+
+        if (!regex.test(url)) {
+            throw new Error("Malformed url. Please provide a correct url format.");
+        }
+        this._newsUrl = url;
+        this._newsData = [];
+        this._fetchDataCallback = fetchDataCallback;
+
+        setInterval(() => {
+            this.fetchNews(this._fetchDataCallback);
+        }, 1000 * 60 * 3);
+        console.info('Constructor executed correctly.')
+    }
+
+    get newsUrl() {
+        return this._newsUrl
+    }
+
+    get newsData() {
+        return this._newsData;
+    }
+
+    fetchNews() {
+        return fetch(this.newsUrl)
+            .then((response) => {
+                return response.json();
+            })
+            .then( ({ news }) => {
+                this._newsData = news;
+                this._fetchDataCallback();
+            })
+            .catch(err => {
+                console.log('Error downloading news');
+            });
+    }
+}
